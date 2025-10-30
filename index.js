5aa1d0ee3a6801093173ff00754aac3e210a9c77
rohit03deogharkar <rd5203327@gmail.com>
2025-10-30 22:20:47 +0530

    initialized chat | socket rooms, authentication

--- index.js
+++ index.js
@@ -5,22 +5,35 @@ const {
   SERVER_PORT,
   LOGGER_ENABLE = false,
   ENVIRONMENT,
+  UI_ORIGIN,
 } = process.env;
 
 const express = require("express");
-const app = express();
+const http = require("http");
 const cors = require("cors");
-const log = require("./logger");
-global.log = log;
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
 
 const { connectDB } = require("./connections/mongodb");
+const log = require("./logger");
+const SocketServer = require("./socketServer/socket");
+const { initializeQueue } = require("./messageQueue/producer");
+const initializeQueueListener = require('./messageQueue/consumer')
+
+const io = new Server(server, {
+  cors: {
+    origin: UI_ORIGIN,
+  },
+});
 
 const userRoutes = require("./routes/user");
 const todoRoutes = require("./routes/todo");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: UI_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -30,6 +43,8 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/todos", todoRoutes);
+
+global.log = log;
 
 app.get("/", (req, res) => {
   global.log("home route hit....");
@@ -43,11 +58,14 @@ const init = async () => {
     console.log("LOGGER DISABLED !!!");
   }
 
-  app.listen(SERVER_PORT, () => {
+  server.listen(SERVER_PORT, () => {
     global.log(`${ENVIRONMENT} server started on port`, SERVER_PORT);
   });
 
+  new SocketServer(io);
   await connectDB();
+  initializeQueue();
+  initializeQueueListener(io)
 };
 
 init();
--- logger.js
+++ logger.js
@@ -1,11 +1,12 @@
-const moment = require("moment");
+// const moment = require("moment");
+const dayjs = require('dayjs')
 
 const { LOGGER_ENABLE = false, ENVIRONMENT } = process.env;
 
 const log = (message, data) => {
   if (LOGGER_ENABLE == "true") {
     if (ENVIRONMENT == "development") {
-      const time = moment().format("YYYY-MM-DD HH:mm:ss.SSS");
+      const time = dayjs().format("YYYY-MM-DD HH:mm:ss.SSS");
       console.log(time + " | " + message + " ==> ", data ? data : "");
     } else {
       console.log(message + " ==> ", data ? data : "");
